8f05da1bd2c6e09e0f0425153a3194deb987bb0a
Nababratadeb-NITA <nababrata13@gmail.com>
2023-08-31 13:12:07 +0530

    conflicts done

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,20 +1,25 @@
 import { Navbar } from "@/components/Nabvar";
 import { Sidebar } from "@/components/Sidebar";
 
-const RootLayout = ({
-    children
-}: {children: React.ReactNode}) => {
-    return ( 
-        <div className="h-full">
-            <Navbar />
-            <div className=" hidden md:flex mt-16 w-20 flex-col fixed inset-y-0">
-                <Sidebar />
-            </div>
-            <main className=" md:pl-20 pt-16 h-full">
-            {children}
-            </main>
-        </div>
-     );
+
+const RootLayout = async ({
+  children
+}: {
+  children: React.ReactNode;
+}) => {
+  const isPro = true;
+
+  return ( 
+    <div className="h-full">
+      <Navbar isPro={isPro} />
+      <div className="hidden md:flex mt-16 h-full w-20 flex-col fixed inset-y-0">
+        <Sidebar isPro={isPro} />
+      </div>
+      <main className="md:pl-20 pt-16 h-full">
+        {children}
+      </main>
+    </div>
+   );
 }
  
 export default RootLayout;
--- components/Nabvar.tsx
+++ components/Nabvar.tsx
@@ -33,7 +33,7 @@ export const Navbar = ({
       </div>
       <div className="flex items-center gap-x-3">
         {!isPro && (
-          <Button size="sm" variant="premium">
+          <Button  size="sm" variant="premium">
             Upgrade
             <Sparkles className="h-4 w-4 fill-white text-white ml-2" />
           </Button>
